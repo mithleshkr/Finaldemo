da6c8e10426ac00a9e4425337745773764c5d5e7
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-10-06 14:35:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Login from './Screens/Login';
+import Dashboard from './Screens/Dashboard';
+import Food from './Screens/Food';
+import User from './Screens/User';
+import {Route, BrowserRouter as Router} from "react-router-dom";
+
+
+
 
 function App() {
   return (
-    <div>
-      <Login />
-      
-    </div>
+    <Router>
+    <Route path='/' exact component={Login}/>
+    <Route path='/Dashboard' component={Dashboard}/>
+    <Route path='/Food' component={Food}/>
+    <Route path='/User' component={User}/>
+  </Router>
   )
 }
 
