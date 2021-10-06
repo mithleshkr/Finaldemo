9f9e7cf4c6138963b7622dc996be9ae68e5d0070
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-10-06 14:34:01 +0530

    Create Food.js

---
+++ Food.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Tab,Tabs, Button} from '@material-ui/core';
+
+import './Food.css'
+import { useHistory } from 'react-router';
+
+
+function Food() {
+    const history =useHistory();
+    return (
+        <div className="sidebar">
+            <div className="tab" >
+            <br/> <br/> <br/><br/><br/><br/><br/>
+            <Tabs > 
+                <Tab label="Dashboard" onClick={() => history.push("Dashboard")}/>
+            </Tabs>
+            <Tabs value={0}> 
+                <Tab label="Food" />
+            </Tabs>
+            <Tabs>
+                <Tab label="User" onClick={() => history.push("User")} />
+            </Tabs>
+
+            </div>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            
+            <Button variant="contained" color="secondary" onClick={()=>history.push("/")} style={{marginLeft:40}} >Logout</Button>
+
+            
+        </div>
+    )
+}
+
+export default Food
