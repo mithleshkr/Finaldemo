743d275eea50adcd96736886e63cc7c0f8e9fb84
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-10-07 14:21:45 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -6,27 +6,33 @@ import {useHistory} from "react-router-dom";
 function Dashboard() {
     const history=useHistory();
     return (
-        <div className="sidebar">
-            <div className="tab" >
-            <br/> <br/> <br/><br/><br/><br/><br/>
-            <Tabs value={0}> 
-                <Tab label="Dashboard" />
-            </Tabs>
-            <Tabs > 
-                <Tab label="Food" onClick={() => history.push("Food")}/>
-            </Tabs>
-            <Tabs>
-                <Tab label="User" onClick={() => history.push("User")} />
-            </Tabs>
+        <div style={{display:"flex",flex:1,height:"100vh"}}>
+            <div className="sidebar" style={{display:"flex",flex:1,height:"100vh",flexDirection:"column"}}>
+                <div className="tab" style={{display:"flex",flex:8,alignItems:"center",justifyContent:"center"}}>
+                            <Tabs value={0}> 
+                         <Tab label="Dashboard" />
+                       </Tabs>
+                        <Tabs> 
+                         <Tab label="Food" onClick={() => history.push("Food")}/>
+                         </Tabs>
+                         <Tabs  >
+                         <Tab label="User" onClick={() => history.push("User")} />
+                         </Tabs>
+                    
+                </div>
+                <div>
+                <Button  variant="contained" color="secondary" onClick={()=>history.push("/")}  >Logout</Button>
+                </div>
+
 
             </div>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            
-            <Button variant="contained" color="secondary" onClick={()=>history.push("/")} style={{marginLeft:40}} >Logout</Button>
+            <div style={{display:"flex",flex:7,height:"100vh",flexDirection:"column"}}>
+           
+        
 
-            
+            </div>
+           
         </div>
-    )
-}
+    )}
 
 export default Dashboard
