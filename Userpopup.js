41d3d4cb663284af723c72b7e1dcce3ca0ce2ad0
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-10-19 13:28:02 +0530

    Update Userpopup.js

--- Userpopup.js
+++ Userpopup.js
@@ -5,6 +5,7 @@ import { Dialog } from "@material-ui/core";
 import { DialogContent } from "@material-ui/core";
 
 import { DialogTitle } from "@material-ui/core";
+import Userpopform from "./Userpopform";
 
 
 export default function Userpopup() {
@@ -19,14 +20,14 @@ export default function Userpopup() {
   };
 
   return (
-    <div style={{display:"flex",justifyContent:"center",marginTop:550,position:"relative"}}>
+    <div style={{display:"flex",justifyContent:"center",marginTop:600,position:"relative",marginLeft:-600}}>
       <Button  variant="contained" color="primary" onClick={handleClickOpen}>
-        Add Food
+        Add User
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>User Details</DialogTitle>
         <DialogContent>
-            
+            <Userpopform />
         </DialogContent>
       </Dialog>
     </div>
