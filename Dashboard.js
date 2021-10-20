56c8757b1852085a668eb36bd8acd57f9bc39d86
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-10-20 14:19:44 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import {Tab,Tabs, Button} from '@material-ui/core';
 import {useHistory} from "react-router-dom";
 
 
 function Dashboard() {
+
+    const [display, setDisplay] = useState([]);
+    useEffect(()=>{
+        showDetails();
+
+    },[])
+
+    const showDetails = async() => {
+        const res= await fetch("http://localhost:3333/user")
+        .then((res)=>res.json())
+        .then((data)=>setDisplay(data))
+    }
+
+
+
+
     const history=useHistory();
     return (
         <div style={{display:"flex",flex:1,height:"100vh"}}>
@@ -27,19 +43,34 @@ function Dashboard() {
 
             </div>
             <div style={{display:"flex",flex:7,height:"100vh",flexDirection:"column"}}>
+                
                 <div style={{display:"flex",flexDirection:"row",justifyContent:"space-evenly"}}>
-                <div style={{display:"flex",height:"30vh",backgroundColor:"blue",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center"}}>
+                <div style={{display:"flex",height:"30vh",backgroundColor:"whitesmoke",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center"}}>
                     Total Food
                 </div>
 
-                <div style={{display:"flex",height:"30vh",backgroundColor:"blue",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center"}}>
+                <div style={{display:"flex",height:"30vh",backgroundColor:"whitesmoke",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center"}}>
                     Total User
                 </div>
                 </div>
-                <div style={{display:"flex",justifyContent:"center"}}> 
-                <div style={{display:"flex",height:"30vh",backgroundColor:"blue",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center"}}>
-                    Recently Added users
+                
+                        
+                    
+                
+                <div style={{display:"flex",justifyContent:"center"}}>
+                    
+                {display.map(post =>{
+                    return(
+                    
+                <div style={{display:"flex",height:"30vh",backgroundColor:"whitesmoke",width:"400px",alignItems:"center",marginTop:50,justifyContent:"center",flexDirection:"column"}}>
+                    
+                    <p>{post.uname}</p> <br />
+                    <p>{post.username}</p>
+                    <p>{post.he}</p>
                 </div>
+                
+                    )
+                })}
                 </div>
 
         
